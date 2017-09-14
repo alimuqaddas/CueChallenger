16ffe54b8e6b5a3485961a4c23abe07801a5b7b6
Ali Muqaddas <alimuqaddas92@gmail.com>
2017-09-14 15:09:07 +0500

    Code Commit 1
    First Code Commit

---
+++ getdata.js
@@ -0,0 +1,171 @@
+var jsonfile = require('jsonfile');
+var ui = require('jquery-ui');
+var combinations = require('combinations');
+var shuffle = require('array-shuffle');
+var random = require('random-number-generator');
+var challenges=[];
+var file='data/challenges.json';
+var currentChallengeId;
+
+var playersInput=1;
+
+
+$( document ).ready(function() {
+    getJsonData();    
+});
+
+
+
+
+function getJsonData(){
+
+    jsonfile.readFile(file, function(err, obj) {
+        console.dir(obj)
+        challenges=obj;
+        challenges.forEach(function(index,item){
+            var t = document.getElementById('challengesBody');
+            var tr = t.insertRow();
+            var td1 = tr.insertCell();
+            var td2 = tr.insertCell();
+            var td3 = tr.insertCell();
+            td1.appendChild(document.createTextNode(index.name));
+            td2.appendChild(document.createTextNode(index.table));
+            td3.appendChild(document.createTextNode(index.playedMatches));
+            tr.addEventListener('click', showChallenge.bind(null, item));
+        })       
+      }.bind(this));
+
+}
+
+
+//When a challenge is selected from the table
+function showChallenge(challengeID) {
+    currentChallengeId=challengeID;
+    $("#home").css("display","none");
+    $("#challengeProfile").css("display","block");
+    var t = document.getElementById('challengeDetails');
+    t.innerHTML="";
+
+    challenges[challengeID].players.forEach(function(index,item){
+        var t = document.getElementById('challengeDetails');
+        var tr = t.insertRow();
+        var td1 = tr.insertCell();
+        var td2 = tr.insertCell();
+        var td3 = tr.insertCell();
+        td1.appendChild(document.createTextNode(index.name));
+        td2.appendChild(document.createTextNode(index.allowedLosses));
+        td3.appendChild(document.createTextNode(index.wins));
+        //tr.addEventListener('click', showChallenge.bind(null, item));
+    }) 
+    console.log(challenges[challengeID].match.player1);
+    $("#player1").html(challenges[challengeID].match.player1);
+    $("#player2").html(challenges[challengeID].match.player2);
+    var active;    
+    if(active)active=document.getElementsByClassName("selected")[0].innerHTML;
+    console.log(active);  
+    }
+
+
+
+function submitmatchResult(){
+    var winner=document.getElementsByClassName("selected")[0].innerHTML;
+    var looser=document.getElementsByClassName("unselected")[0].innerHTML;
+    $(document.getElementsByClassName("selected")[0]).addClass("unselected");
+    $(document.getElementsByClassName("selected")[0]).removeClass("selected");
+    challenges[currentChallengeId].playedMatches=parseInt(challenges[currentChallengeId].playedMatches)+1;
+
+    challenges[currentChallengeId].players.forEach(function(player,index){
+        if(player.name==winner)
+            {
+                console.log("Before Add:",player.wins)
+                player.wins=parseInt(player.wins)+1;
+                console.log("After Add:",player.wins);
+            }
+        else if(player.name==looser)
+            {
+                console.log("Before Minus:",player.allowedLosses)
+                player.allowedLosses=parseInt(player.allowedLosses)-1;
+                console.log("After Add:",player.allowedLosses)
+            }
+    })
+    console.log(challenges);
+    setMatch();
+    
+    jsonfile.writeFile(file, challenges, function (err) {
+        console.error(err);
+        
+        jsonfile.readFile(file, function(err, obj) {
+            console.dir(obj)
+            challenges=obj;
+            var t = document.getElementById('challengesBody');
+            t.innerHTML="";
+            challenges.forEach(function(index,item){
+        
+                //STATIC EVENTS
+                var t = document.getElementById('challengesBody');
+                var tr = t.insertRow();
+                var td1 = tr.insertCell();
+                var td2 = tr.insertCell();
+                var td3 = tr.insertCell();
+                td1.appendChild(document.createTextNode(index.name));
+                td2.appendChild(document.createTextNode(index.table));
+                td3.appendChild(document.createTextNode(index.playedMatches));
+                tr.addEventListener('click', showChallenge.bind(null, item));
+                showChallenge(currentChallengeId);
+
+            })      
+            }.bind(this));
+      });
+      alert("Match Updated !!! New Match have been generated randomly, Start Cueing..");
+    
+    
+}
+
+function setMatch(){
+    var currentPossiblePlayers=[];
+
+    challenges[currentChallengeId].players.forEach(function(player,index){
+        if(parseInt(player.allowedLosses)>0)
+        {
+            currentPossiblePlayers.push(player.name);
+        }
+    })
+    var comb=combinations(currentPossiblePlayers,2);
+    console.log("Possible Players",currentPossiblePlayers);
+    console.log("Conmibations",comb);
+    console.log("Conmibations",combinations(currentPossiblePlayers,2));
+    var counter=0;
+    var possibleMatches=[];
+    comb.forEach(function(value,index){
+        if(value.length<=2)
+            {
+                possibleMatches.push(value);
+                counter++;
+            }
+    })
+    console.log("beforeShuffle:",possibleMatches)
+    var aftershuffle=shuffle(possibleMatches);
+    console.log("After Shuffle",aftershuffle);
+    console.log("counter:",possibleMatches.length);
+    console.log(possibleMatches);
+    var rand=random(aftershuffle.length-1,0)
+    console.log(rand);
+
+    challenges[currentChallengeId].match.player1=aftershuffle[rand][0];
+    challenges[currentChallengeId].match.player2=aftershuffle[rand][1];
+
+}
+
+function newChallenge(){
+    hideforchallege();
+    //document.getElementById("playersAddField").innerHTML="<input type=\"text\" id=\""+playersInput+"\" placeholder=\"Enter Player Name\">";
+    playersInput++;
+}
+
+function createChallenge(){
+
+    
+
+}
+
+
---
+++ helpingfunctions.js
@@ -0,0 +1,64 @@
+// Helping Function
+function onRowClick(tableId, callback) {
+    var table = document.getElementById(tableId),
+        rows = table.getElementsByTagName("tr"),
+        i;
+    for (i = 0; i < rows.length; i++) {
+        table.rows[i].onclick = function (row) {
+            return function () {
+                callback(row);
+            };
+        }(table.rows[i]);
+    }
+};
+
+
+$("#player1").click(function(){
+    $("#player1").removeClass("unselected");
+    $("#player1").addClass("selected")
+    if($("#player2").hasClass("selected"))
+        {
+            $("#player2").removeClass("selected");
+            $("#player2").addClass("unselected")
+        }
+    });
+
+$("#player2").click(function(){
+    $("#player2").removeClass("unselected");
+    $("#player2").addClass("selected")
+    if($("#player1").hasClass("selected"))
+        {
+            $("#player1").removeClass("selected");
+            $("#player1").addClass("unselected");
+        }
+    });
+
+function gohome(){
+    $("#challengeProfile").css("display","none");
+    $("#home").css("display","block");
+}
+
+function hideforchallege(){
+    $("#home").css("display","none");
+    $("#createChallenge").css("display","block");
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+function addPlayer(){
+    //$('#playersAddField').add("<input type=\"text\" id=\""+playersInput+"_name\"style=\"display:inline-block\" placeholder=\"Player Name\"/>");
+    //$('#playersAddField').add("<input type=\"text\" id=\""+playersInput+"_defeats\"style=\"display:inline-block\" placeholder=\"Allowed Defeats\"/>");
+    document.getElementById("playersAddField").innerHTML=document.getElementById("playersAddField").innerHTML+"<input type=\"text\" id=\""+playersInput+"_name\" placeholder=\"Player Name\" style=\"display:inline-block\"/>";
+    document.getElementById("playersAddField").innerHTML=document.getElementById("playersAddField").innerHTML+"<input type=\"text\" id=\""+playersInput+"_defeats\" placeholder=\"Allowed Defeats\" style=\"display:inline-block\"/>";
+    playersInput++;
+}
+
+function removePlayer(){
+    $('#playersAddField').children().last().remove();
+    $('#playersAddField').children().last().remove();
+    playersInput--;
+}
